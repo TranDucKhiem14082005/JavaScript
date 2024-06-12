90fa5217738755edb2fa2b1b900d450722d7199c
TranDucKhiem14082005 <khiemtran015@gmail.com>
2024-06-13 00:31:42 +0700

    pactice-JS

---
+++ main2.js
@@ -0,0 +1,79 @@
+// var age = 18;
+// console.log(typeof age)
+// var fullName = 'Tran Duc Khiem'
+// console.log(typeof fullName)
+
+// var check;
+// console.log(typeof check)
+
+// var ktr = true;
+// console.log(typeof ktr)
+
+// var isNull = null;
+// console.log(typeof isNull)
+
+// var id = Symbol('id')
+
+// console.log(id);
+// console.log(typeof id)
+
+
+// var fullName = '         Tran Duc Khiem!     ';
+// console.log(fullName.length)
+
+// var firstName = 'Khiem';
+// var lastName ='Tran';
+// console.log(`Toi la: ${firstName}${'-'}${lastName}`)
+
+// console.log(fullName.indexOf('Duc'));
+
+// console.log(fullName.indexOf('Duc',8))
+
+// console.log(fullName.lastIndexOf('Duc'))
+
+// console.log(fullName.search('Duc'))
+
+// console.log(fullName.slice(5,8))
+
+// console.log(fullName.slice(5))
+
+// console.log(fullName.slice(-6,-1))
+
+// console.log(fullName.replace('Duc','duc'))
+// console.log(fullName.replace(/Duc/g,'duc'))
+// console.log(fullName.toUpperCase())
+// console.log(fullName.toLowerCase())
+// console.log(fullName.trim())
+// console.log(fullName.trim().length)//do dai cua chuoi sau khi chuan hoa
+// console.log(fullName.split())
+// console.log(fullName.split(''))
+// console.log(fullName.charAt(14))//lay mot ki tu boi 1 index co truoc
+// console.log(fullName[10])//lay 1 ki tu cach 2
+
+
+// var age = 18;
+// var pi=3.145636
+
+// console.log(isNaN(age));
+
+// console.log(age.toString())
+// console.log((pi.toFixed(3)))
+// console.log(typeof pi.toFixed(3))
+
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+]
+
+console.log(languages);
+console.log(typeof languages);
+console.log(Array.isArray(languages));
+console.log(languages.length);
+console.log(languages[2]) //cap nhat dia chi khong se tra ve undefined
+console.log(languages.toString());
+console.log(typeof languages.toString());
+console.log(languages.pop())
+console.log(languages)
+console.log(languages.push('Dart'))
+console.log(languages)
